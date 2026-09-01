3e71462e37738690ecb991c7f06d45af569de5ec
4U.IA.BR <65726431+4u-Labs@users.noreply.github.com>
2026-09-01 15:44:37 -0300

    feat: Adiciona sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'atari-2600-v1.0';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  // Network-First strategy
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
